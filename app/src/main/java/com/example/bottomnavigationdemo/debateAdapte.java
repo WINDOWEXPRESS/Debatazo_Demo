4271bd489f5ad85ddbcaf8ad77e87da2310aa4f1
Xing <xing5539@gmail.com>
2024-01-22 23:07:00 +0100

    Version con logo,recycleview de debate sin terminal

---
+++ app/src/main/java/com/example/bottomnavigationdemo/debateAdapte.java
@@ -0,0 +1,76 @@
+package com.example.bottomnavigationdemo;
+
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public class debateAdapte extends RecyclerView.Adapter<debateAdapte.debateViewHolder> {
+
+    private List<debateItem> listaDebate;
+    public debateAdapte(List<debateItem> listaDebate) {
+        this.listaDebate = listaDebate;
+    }
+    public static class debateViewHolder extends RecyclerView.ViewHolder{
+        imagenCircular perfil;
+        TextView nombre;
+        TextView fecha;
+        TextView contenido;
+        ImageView image;
+
+        public debateViewHolder(@NonNull View itemView) {
+            super(itemView);
+            perfil = itemView.findViewById(R.id.debateRV_imagenC_usuario);
+            nombre = itemView.findViewById(R.id.debateRV_textV_nombre);
+            fecha = itemView.findViewById(R.id.debateRV_textV_fecha);
+            contenido = itemView.findViewById(R.id.debateRV_textV_contenido);
+            image = itemView.findViewById(R.id.debateRV_imageV);
+        }
+    }
+    @NonNull
+    @Override
+    public debateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.debate_recycler_view,parent,false
+        );
+        return new debateViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull debateViewHolder holder, int position) {
+        debateItem debateItem = listaDebate.get(position);
+        URL perfilUrl = null,contenidoUrl = null;
+        Bitmap perfilBitmap = null,contenidoBitmap = null;
+        try {
+            perfilUrl = new URL(debateItem.imagenUsuario);
+            perfilBitmap = BitmapFactory.decodeStream(perfilUrl.openStream());
+            contenidoUrl = new URL(debateItem.imagenUrl);
+            contenidoBitmap = BitmapFactory.decodeStream(contenidoUrl.openStream());
+        }catch (Exception e){}
+
+        holder.perfil.setImageBitmap(perfilBitmap);
+        holder.nombre.setText(debateItem.nombreUsuario);
+        holder.fecha.setText(new SimpleDateFormat("dd/mm/yyyy").format(debateItem.fechaPublicacion));
+        holder.contenido.setText(debateItem.contenido);
+        holder.image.setImageBitmap(contenidoBitmap);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+}
---
+++ app/src/main/java/com/example/bottomnavigationdemo/debateItem.java
@@ -0,0 +1,41 @@
+package com.example.bottomnavigationdemo;
+
+import java.util.Date;
+
+public class debateItem {
+    Integer idUsuario;
+    Integer idDebate;
+    String imagenUsuario;
+    Date fechaPublicacion;
+    String nombreUsuario;
+    String contenido;
+    String imagenUrl;
+
+    public debateItem(Integer idUsuario, Integer idDebate, String imagenUsuario, Date fechaPublicacion, String nombreUsuario, String contenido, String imagenUrl) {
+        this.idUsuario = idUsuario;
+        this.idDebate = idDebate;
+        this.imagenUsuario = imagenUsuario;
+        this.fechaPublicacion = fechaPublicacion;
+        this.nombreUsuario = nombreUsuario;
+        this.contenido = contenido;
+        this.imagenUrl = imagenUrl;
+    }
+    public String getImagenUsuario(){
+        return imagenUsuario;
+    }
+    public Date getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+}
