17b58096df4fdbb5ae371bc70d72835f2179025b
BIG BOSS <zhouyichen1998@gmail.com>
2024-01-29 22:14:09 +0100

    aniadir actividad Configuracion y datos personal con el vinculo con framentoPerfil (no implementar todas las logicas)

---
+++ app/src/main/java/com/example/debatazo/perfilylogin/configuracion/Configuracion.java
@@ -0,0 +1,16 @@
+package com.example.debatazo.perfilylogin.configuracion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.debatazo.R;
+
+public class Configuracion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_configuracion);
+    }
+}
